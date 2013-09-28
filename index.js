f963eac667d19d301aacd085a9f8b8db560ee173
Damian Krzeminski <pirxpilot@code42day.com>
2013-09-28 18:10:50 -0400

    Support defining 'star' character
    Instead of standard unicode star you can use other character in standard
    fonts: like ❤  '&#x2764'
    Or you can use character in your own font (just define font-family for
    .rating class)
    
    We are not using domify any more since we need to create HTML
    dynamically to support setting star character.

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
-var html = require('./template');
-var domify = require('domify');
-
 module.exports = rating;
 
-function rating(el, r) {
-	var self = {},
-		node = domify(html);
 
+function content(star) {
+	var i, c = [];
+	for(i = 0; i < 5; i++) {
+		c[i] = star;
+	}
+	return c.join('');
+}
+
+function div(className, content) {
+	var node = document.createElement('div');
+	node.className = className;
+	if (content) {
+		node.innerHTML = content;
+	}
+	return node;
+}
+
+/**
+ * creates rating HTML that looks like this:
+ *		div.rating
+ *			div.shadow= ★★★★★
+ *			div.stars=  ★★★★★
+ */
+function create(star) {
+	var c, node;
+
+	c = content(star);
+	node = div('rating');
+	node.appendChild(div('shadow', c));
+	node.appendChild(div('stars', c ));
+
+	return node;
+}
+
+/**
+ * Creates rating component
+ * @param el {Node} - DOM node to which we will append rating
+ * @param options {Object} - optional {star, value} object when
+ *     `star` is the character displayed in rating and `value` is the initial rating
+ */
+function rating(el, options) {
+	var self = {}, node;
+
+	if (typeof options === 'number') {
+		options = {
+			value: options
+		};
+	} else {
+		options = options || {};
+	}
+	options.star = options.star || '&#9733;';
+	options.value = options.value || 0;
+
+	node = create(options.star);
 	el.appendChild(node);
-	if (typeof r === 'number') {
-		set(r);
+	if (options.value) {
+		set(options.value);
 	}
 
 	function set(r) {
 		var style = 'width:' + r + '%;';
-		node.querySelector('.stars').setAttribute('style', style);
+		node.childNodes[1].setAttribute('style', style);
 		return self;
 	}
 
