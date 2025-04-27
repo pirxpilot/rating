a2ec82411beefad67d08dac33864a7461266ba50
Damian Krzeminski <pirxpilot@furkot.com>
2025-04-26 21:29:33 -0600

    modernize code

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 module.exports = rating;
 
-function content(star) {
-  let i;
-  const c = [];
-  for (i = 0; i < 5; i++) {
-    c[i] = star;
-  }
-  return c.join('');
-}
+/**
+ * Creates rating component
+ * @param el {Node} - DOM node to which we will append rating
+ * @param options {Object} - optional {star, value} object when
+ *     `star` is the character displayed in rating and `value` is the initial rating
+ */
+function rating(el, options = {}) {
+  const self = {
+    set
+  };
 
-function div(className, content) {
-  const node = document.createElement('div');
-  node.className = className;
-  if (content) {
-    node.innerHTML = content;
+  if (typeof options === 'number') {
+    options = {
+      value: options
+    };
   }
-  return node;
+  options.star ??= '&#9733;';
+  options.value ??= 0;
+
+  const node = create(options.star);
+  el.appendChild(node);
+  if (options.value) {
+    set(options.value);
+  }
+
+  return self;
+
+  function set(r) {
+    node.childNodes[1].setAttribute('style', `width:${r}%;`);
+    return self;
+  }
 }
 
 /**
@@ -25,7 +40,7 @@ function div(className, content) {
  *			div.stars=  ★★★★★
  */
 function create(star) {
-  const c = content(star);
+  const c = star.repeat(5);
   const node = div('rating');
   node.appendChild(div('shadow', c));
   node.appendChild(div('stars', c));
@@ -33,38 +48,11 @@ function create(star) {
   return node;
 }
 
-/**
- * Creates rating component
- * @param el {Node} - DOM node to which we will append rating
- * @param options {Object} - optional {star, value} object when
- *     `star` is the character displayed in rating and `value` is the initial rating
- */
-function rating(el, options) {
-  const self = {};
-
-  if (typeof options === 'number') {
-    options = {
-      value: options
-    };
-  } else {
-    options = options || {};
+function div(className, content) {
+  const node = document.createElement('div');
+  node.className = className;
+  if (content) {
+    node.innerHTML = content;
   }
-  options.star = options.star || '&#9733;';
-  options.value = options.value || 0;
-
-  const node = create(options.star);
-  el.appendChild(node);
-  if (options.value) {
-    set(options.value);
-  }
-
-  function set(r) {
-    const style = 'width:' + r + '%;';
-    node.childNodes[1].setAttribute('style', style);
-    return self;
-  }
-
-  self.set = set;
-
-  return self;
+  return node;
 }
